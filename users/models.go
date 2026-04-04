622dacd5801183e2185221a6119b2397bb34b6f4
Li Kui <90845831+likui628@users.noreply.github.com>
2026-04-04 21:09:12 +0800

    chore: package structure

--- users/models.go
+++ users/models.go
@@ -3,10 +3,12 @@ package users
 import "gorm.io/gorm"
 
 type UserModel struct {
-    gorm.Model
-    Username string `gorm:"uniqueIndex"`
-    Email    string `gorm:"uniqueIndex"`
-    Password string 
+	ID           uint    `gorm:"primaryKey"`
+	Username     string  `gorm:"column:username"`
+	Email        string  `gorm:"column:email;uniqueIndex"`
+	Bio          string  `gorm:"column:bio;size:1024"`
+	Image        *string `gorm:"column:image"`
+	PasswordHash string  `gorm:"column:password;not null"`
 }
 
 func AutoMigrate(db *gorm.DB) {
--- users/routers.go
+++ users/routers.go
@@ -1,7 +1,25 @@
 package users
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/likui628/golang-gin-realworld-example-app/database"
+	"net/http"
+)
 
 func UsersRegister(router *gin.RouterGroup) {
-    router.POST("/", Registration)
+    router.POST("", UsersRegistration)
+	router.POST("/", UsersRegistration)
+}
+
+
+func UsersRegistration(c *gin.Context) {
+    var user UserModel
+    if err := c.ShouldBindJSON(&user); err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+        return
+    }
+
+    database.DB.Create(&user)
+    
+    c.JSON(http.StatusOK, gin.H{"message": "registration success"})
 }
